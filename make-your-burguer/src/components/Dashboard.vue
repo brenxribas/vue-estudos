ecdcb8f6e8bd005e2fd34cd3a26936afbf33d1c7
brenxribas <breno_ribas@outlook.com>
2022-07-10 20:37:13 -0300

    listando dados do db

--- make-your-burguer/src/components/Dashboard.vue
+++ make-your-burguer/src/components/Dashboard.vue
@@ -12,15 +12,14 @@
     </div>
 
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>Joao</div>
-        <div>Integral</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{burger.id}}</div>
+        <div>{{burger.nome}}</div>
+        <div>{{burger.pao}}</div>
+        <div>{{burger.carne}}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">{{opcional}}</li>
           </ul>
         </div>
         <div>
@@ -37,6 +36,26 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+        burgers: null,
+        burger_id: null,
+        status: []
+        }
+    },
+    methods: {
+        async getPedidos(){
+            const req = await fetch("http://localhost:3000/burgers");
+            const data = await req.json();
+            this.burgers = data;            
+            console.log(this.burgers);
+
+        }
+    },
+    mounted() {
+        this.getPedidos();
+    },
+  
 };
 </script>
 
@@ -61,43 +80,39 @@ export default {
 
 #burger-table-heading div,
 .burger-table-row div {
-    width: 19%;
+  width: 19%;
 }
 
 .burger-table-row {
-    width: 100%;
-    padding: 12px;
-    border-bottom: 1px solid #CCC;
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
 }
 
 #burger-table-heading .order-id,
 .burger-table-row .order-number {
-    width: 5%;
+  width: 5%;
 }
 
 select {
-    padding: 12px 6px;
-    margin-right: 12px;
+  padding: 12px 6px;
+  margin-right: 12px;
 }
 
 .delete-btn {
-    background-color: #222;
-    color: #FCBA03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
 }
 
-.delete-btn:hover{
-    background-color: transparent;
-    color: #222;
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
 }
-
-
-
-
 </style>
